ba2f47991e2d0d9e1fa720d3a1437d5a0373edfd
Lukasz Obara <lukasz48@gmail.com>
2017-01-27 14:15:43 -0500

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -4,8 +4,7 @@ import numpy as np
 import pandas as pd
 import scipy.ndimage
 
-location = 'C:\\Users\\lukas\\OneDrive\\Documents\\Machine Learning\\Kaggle'+\
-		   '\\MNIST\\train.csv'
+location = 'C:\\...\\MNIST\\train.csv'
 
 data_all = pd.read_csv(location)
 # digits = data_all.iloc[:, 1:].astype(np.uint8)
@@ -71,47 +70,6 @@ for i, aug in enumerate(augmented_data):
 test = np.append(augmented_data, test, axis=0)
 np.random.shuffle(test)
 
-location1 = 'C:\\Users\\lukas\\OneDrive\\Documents\\Machine Learning\\Kaggle'+\
-			'\\MNIST\\augmented_train.npy'
+location1 = 'C:\\...\\MNIST\\augmented_train.npy'
 
 np.save(location1, test)
-
-# label_temp = np.asarray(test1[0])
-# number_temp = test1[1:]
-# number_temp = number_temp.reshape(28, 28)
-# print(test.shape)
-# test_rot = scipy.ndimage.rotate(number_temp, -15, reshape=False)
-# test_rot = test_rot.reshape(784,)
-# data_temp = np.append(label_temp, test_rot)
-# data_temp = data_temp.reshape(1, -1)
-# print(data_temp.shape)
-# test = np.append(test, data_temp, axis=0)
-# print(test_rot.shape)
-# test_shift = scipy.ndimage.shift(test, (0, 5)) # Shifts (y, x) positive is down in y direction
-# print(test_shift.shape)
-# test_stretch = scipy.ndimage.interpolation.zoom(test, (1, 1.3)) # needs to be a number between 1 and 2, >2 will not fit properly
-# print(test_stretch.shape)
-# test_stretch = test_stretch[:, 4: -4]
-# print(test_stretch.shape)
-
-# # plt.figure(figsize=(10,10))
-# # plt.imshow(test, cmap=cm.binary)
-# # plt.show()
-
-# fig, axarr = plt.subplots(nrows=1, ncols=2) 
-# axarr[0].imshow(number_temp, cmap=cm.binary)
-# axarr[0].set_title('Original')
-# axarr[1].imshow(test_rot, cmap=cm.binary)
-# axarr[1].set_title('Rotation')
-# axarr[2].imshow(test_shift, cmap=cm.binary)
-# axarr[2].set_title('Shift')
-# axarr[3].imshow(test_stretch, cmap=cm.binary)
-# axarr[3].set_title('Strech')
-# plt.show()
-# for i in range(10):
-# 	axarr[i].imshow(digits[i][0], cmap=cm.binary)
-# 	axarr[i].axis('off')
-
-# for i in range(10):
-# 	plt.imshow(digits[i][0], cmap=cm.binary)
-# plt.show()
